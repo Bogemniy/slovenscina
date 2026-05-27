187447b5336a49438fc062aeb480c40dad4e6e19
Bogemniy <bogemniyprincess@gmail.com>
2026-05-27 19:50:38 +0300

    data: add 15 words (animals, patterns, clothing)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump CACHE_VERSION to invalidate clients on deploy.
-const CACHE_VERSION = "v29";
+const CACHE_VERSION = "v30";
 const APP_SHELL = `slo-shell-${CACHE_VERSION}`;
 const DATA_CACHE = `slo-data-${CACHE_VERSION}`;
 
