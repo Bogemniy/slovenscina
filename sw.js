59f6e0e074c6649f0dde9a975a6b8f39a9a7d53f
Bogemniy <bogemniyprincess@gmail.com>
2026-05-31 15:56:05 +0300

    data: add 70 words (dialogue particles, food, home, colors, materials)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump CACHE_VERSION to invalidate clients on deploy.
-const CACHE_VERSION = "v33";
+const CACHE_VERSION = "v34";
 const APP_SHELL = `slo-shell-${CACHE_VERSION}`;
 const DATA_CACHE = `slo-data-${CACHE_VERSION}`;
 
