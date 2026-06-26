b4be02476708e1498b175efe42b435e3b4a43fca
Bogemniy <bogemniyprincess@gmail.com>
2026-06-26 10:46:23 +0300

    grammar: add koliko-je-ura topic (pol/četrt/čez/do)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump CACHE_VERSION to invalidate clients on deploy.
-const CACHE_VERSION = "v91";
+const CACHE_VERSION = "v92";
 const APP_SHELL = `slo-shell-${CACHE_VERSION}`;
 const DATA_CACHE = `slo-data-${CACHE_VERSION}`;
 
